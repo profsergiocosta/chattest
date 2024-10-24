34a883a3a7c6e77f279365ab262e0a307e61d248
Sergio Costa <sergio.costa@ufma.br>
2024-10-24 11:45:11 -0300

    update

--- app.py
+++ app.py
@@ -5,15 +5,6 @@ import chainlit as cl
 def on_chat_start():
     print("A new chat session has started!")
 
-#@cl.password_auth_callback
-def auth_callback(username: str, password: str):
-    # Verifica o usuário e senha
-    if (username, password) == ("admin", "admin"):
-        return cl.User(
-            identifier="admin", metadata={"role": "admin", "provider": "credentials"}
-        )
-    else:
-        return None
 
 
 @cl.on_message
